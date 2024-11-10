530c620c378a97e92bd125f267d14715253a89e8
wzk <1356422517@qq.com>
2024-11-10 21:14:11 +0800

    2024.11.10目录调整

--- 前端手写题/原生ajax.js
+++ 前端手写题/原生ajax.js
@@ -1,19 +1,20 @@
 // 创建 Ajax 对象
-const XHR = new XMLHttpRequest();
+const XHR = new XMLHttpRequest()
 
 // 告诉 Ajax 请求地址以及请求方法
-XHR.open("GET", "url");   //第三个参数默认是true是异步  false是同步
+XHR.open("GET", "url")
 
 // 发送请求
-XHR.send();
+XHR.send()  // 如果是POST 这里传入请求参数
 
 // 获取服务器的响应数据
 XHR.onload = function () {
   if (XHR.status === 200) {
-    console.log(JSON.parse(XHR.responseText));
+    console.log(JSON.parse(XHR.responseText))
   } else {
-    console.log(XHR.statusText);
+    console.log(XHR.statusText)
   }
-};
+}
 
 // XHR.setRequestHeader("Accept", "application/json");
+// XHR.abort() // 取消请求
